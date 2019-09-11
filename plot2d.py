4ada331ab878afbc5ee638862e27def439e4e937
Matheus Vinícius <mvtna@cin.ufpe.br>
2019-09-11 15:49:13 -0300

    correct code to plot a graph

--- plot2d.py
+++ plot2d.py
@@ -16,15 +16,15 @@ def plot2d(fl):
 
         for row in plots:
             time.append(float(row[0]))
-            axis_y.append(float(row[1]))
-            axis_x.append(float(row[2]))
+            axis_x.append(float(row[1]))
+            axis_y.append(float(row[2]))
             axis_z.append(float(row[3]))
 
     displacement = get_offset(axis_x, axis_y, axis_z)
 
     print(f'len(time) = {len(time)}  len(x) = {len(axis_x)} :: len(y) = {len(axis_y)} :: len(z) = {len(axis_z)} :: len(offset) = {len(displacement)}')
 
-    plot(time, axis_z, 'Time', 'Displacement', 'magenta', 'Gazebo', 'gazebo')
+    plot(time, axis_y, 'Time (s)', 'Z (m)', 'magenta', 'Gazebo', 'gazebo')
 
 def plot(axis_x, axis_y, xlabel, ylabel, graph_color, graph_label, name):
 
@@ -41,6 +41,7 @@ def plot(axis_x, axis_y, xlabel, ylabel, graph_color, graph_label, name):
 def get_offset(x, y, z):
 
     displacement = []
+    displacement.append(0)
 
     for i in range(1, len(x)):
         offset  = pow(x[i] - x[i-1], 2)
