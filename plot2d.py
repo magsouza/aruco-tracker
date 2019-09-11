e338e32fa023e7839e07a17f62a610f85483f93b
Magnon Souza <mprs@cin.ufpe.br>
2019-09-11 16:00:12 -0300

    plotting 2 files

--- plot2d.py
+++ plot2d.py
@@ -5,26 +5,26 @@ import pylab
 
 pylab.rcParams['figure.figsize'] = (8.0, 8.0)
 
-def plot2d(fl):
-    axis_x = []
-    axis_y = []
-    axis_z = []
-    time   = []
+def plot2d(fl1, fl2):
+    for fl in [fl1, fl2]:
+        axis_x = []
+        axis_y = []
+        axis_z = []
+        time   = []
+        with open(fl) as csvfile:
+            plots = csv.reader(csvfile, delimiter=' ')
 
-    with open(fl) as csvfile:
-        plots = csv.reader(csvfile, delimiter=' ')
+            for row in plots:
+                time.append(float(row[0]))
+                axis_x.append(float(row[1]))
+                axis_y.append(float(row[2]))
+                axis_z.append(float(row[3]))
 
-        for row in plots:
-            time.append(float(row[0]))
-            axis_x.append(float(row[1]))
-            axis_y.append(float(row[2]))
-            axis_z.append(float(row[3]))
+        displacement = get_offset(axis_x, axis_y, axis_z)
 
-    displacement = get_offset(axis_x, axis_y, axis_z)
+        print(f'len(time) = {len(time)}  len(x) = {len(axis_x)} :: len(y) = {len(axis_y)} :: len(z) = {len(axis_z)} :: len(offset) = {len(displacement)}')
 
-    print(f'len(time) = {len(time)}  len(x) = {len(axis_x)} :: len(y) = {len(axis_y)} :: len(z) = {len(axis_z)} :: len(offset) = {len(displacement)}')
-
-    plot(time, axis_y, 'Time (s)', 'Z (m)', 'magenta', 'Gazebo', 'gazebo')
+        plot(time, axis_y, 'Time (s)', 'Z (m)', 'magenta', 'Gazebo', 'gazebo')
 
 def plot(axis_x, axis_y, xlabel, ylabel, graph_color, graph_label, name):
 
